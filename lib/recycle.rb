da9c4d39bdfcd209e9662ddbf5adc270fd02411d
samyuravikumar <samyuktha321@gmail.com>
2018-07-26 16:31:32 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -49,3 +49,6 @@ end
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
+
+def key_and_data(recycle_bin)
+  
